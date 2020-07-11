83e5737a0859421ccae735aea42349421e02cefc
joeyang <joeyang1512@gmail.com>
2020-07-11 17:31:11 +0800

    首次上传项目

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    lintOnSave: false,
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:80',
+                pathRewrite: { '^/api': '' },
+                secure: false,
+                changeOrigin: true
+            }
+        }
+    }
+}
