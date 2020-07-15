e9c35d0d8beb6bc538a85712562bc6241e96fa40
joeyang1512 <joeyang1512@gmail.com>
2020-07-15 09:46:10 +0800

    提交master

---
+++ src/components/sciFruit/sciFruitRight1.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="col-md-9" style="padding:0">
+      <ul class="breadcrumb" style="margin:20px 0 10px 0px;background-image: linear-gradient(to bottom, rgba(190,220,240,1), rgba(255,250,250,0));" >
+        <li>F309</li>
+        <li style="font-family: '微软雅黑', '黑体'">科研成果</li>
+        <li>科研项目</li>
+      </ul>
+      <div class="table-responsive">
+        <table class="table table-bordered table-hover" style="background-color: #fff;">
+          <thead>
+            <tr>
+              <th>项目名称</th>
+              <th>项目来源</th>
+              <th>起止日期</th>
+              <th>负责人</th>
+            </tr>
+          </thead>
+          <tbody style="font-size: 10px" v-if="info">
+            <!-- <foreach name="scifruits" item="v"> -->
+            <tr v-for="(item,index) in info" :key="'sci'+index">
+              <td style="font-size: 13px">{{item.name}}</td>
+              <td style="font-size: 13px">{{item.origin}}</td>
+              <td style="font-size: 13px">{{item.data}}</td>
+              <td style="font-size: 13px">{{item.duty}}</td>
+            </tr>
+            <!-- </foreach> -->
+          </tbody>
+        </table>
+
+        <nav class="pull-right" style="margin-bottom: 10px">2</nav>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { getSciFruits1 } from "@/api/api";
+// @ is an alias to /src
+export default {
+  name: "sciFruitRight2",
+  data() {
+    return {
+      info:''
+    };
+  },
+  components: {},
+  created() {
+    getSciFruits1().then(res => {
+      this.info = res;
+    });
+  }
+};
+</script>
+<style lang='less'>
+</style>
---
+++ src/components/sciFruit/sciFruitRight2.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <!--右侧-->
+    <div class="col-md-9" style="padding:0">
+      <ul class="breadcrumb" style="margin:20px 0 10px 0px;background-image: linear-gradient(to bottom, rgba(190,220,240,1), rgba(255,250,250,0));" >
+        <li>F309</li>
+        <li style="font-family: '微软雅黑', '黑体'">科研成果</li>
+        <li>科研项目</li>
+      </ul>
+      <div class="table-responsive">
+        <table class="table table-bordered table-hover" style="background-color: #fff;">
+          <thead>
+            <tr>
+              <th>成果名称</th>
+              <th>奖励级别</th>
+              <th>年度</th>
+              <th>完成人</th>
+            </tr>
+          </thead>
+          <tbody style="font-size: 10px" v-if="info">
+            <!-- <foreach name="scirews" item="v"> -->
+              <tr v-for="(item,index) in info" :key="'sci'+index">
+                <td style="font-size: 13px">{{item.name}}</td>
+                <td style="font-size: 13px">{{item.level}}</td>
+                <td style="font-size: 13px">{{item.year}}</td>
+                <td style="font-size: 13px">{{item.endman}}</td>
+              </tr>
+            <!-- </foreach> -->
+          </tbody>
+        </table>
+
+        <nav class="pull-right" style="margin-bottom: 10px">2</nav>
+      </div>
+    </div>
+    <!--end-->
+  </div>
+</template>
+<script>
+import { getSciFruits2 } from "@/api/api";
+// @ is an alias to /src
+export default {
+  name: "sriFruitRight",
+  data() {
+    return {
+      info:''
+    };
+  },
+  components: {},
+  created(){
+    getSciFruits2().then(res=>{
+      this.info = res;
+    })
+  }
+};
+</script>
+<style lang='less'>
+</style>
